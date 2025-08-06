ead5b3e9132913550aab5ed8992596081275fdf5
Zephilinox <Zephilinox@hotmail.co.uk>
2025-08-06 02:20:28 +0100

    Check that resources are correct by using resaver

--- init-and-run-tests.sh
+++ init-and-run-tests.sh
@@ -41,6 +41,17 @@ $GODOT_BIN --headless --editor --render-thread safe --single-threaded-scene --qu
 echo Importing resources
 $GODOT_BIN --import --headless --render-thread safe --single-threaded-scene --quit
 
+echo Checking resources
+$GODOT_BIN --headless --editor --script "addons/resource_resave/resave_resources.gd" --render-thread safe --single-threaded-scene --quit
+
+if [[ -n "$(git status --porcelain)" ]]; then
+  echo "::error::Some .tres or .res files are not up to date. Please run the Resource Resaver (button in the top-right of the Godot Editor) and fix any broken resources!"
+  git diff
+  exit 1
+else
+  echo "All resources are up to date"
+fi
+
 echo Running GUT tests using params:
 echo "  -> $GUT_PARAMS"
 
