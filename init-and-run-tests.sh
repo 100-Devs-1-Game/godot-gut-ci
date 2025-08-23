e974129efcd1a61fdb1d6c3f042aae0a5312d1c5
Zephilinox <Zephilinox@hotmail.co.uk>
2025-08-23 16:52:09 +0100

    Make CI error messages more helpful

--- init-and-run-tests.sh
+++ init-and-run-tests.sh
@@ -47,29 +47,50 @@ echo "  -> $GUT_PARAMS"
 TEMP_FILE=/tmp/gut.log
 $GODOT_BIN -d -s $GODOT_PARAMS --path $PWD addons/gut/gut_cmdln.gd -gexit $GUT_PARAMS --render-thread safe --single-threaded-scene --verbose 2>&1 | tee $TEMP_FILE
 
+echo ""
+echo "#####################"
+echo "       RESULTS       "
+echo "#####################"
+
+FAILED=0
+
 # Godot always exists with error 0, but we want this action to fail in case of errors
 if grep -q "No tests ran" "$TEMP_FILE" || grep -qE "Asserts\s+none" "$TEMP_FILE";
 then
-  echo "No test ran. Please check your 'gut_params'"
-  exit 1
+  echo "CI FAILED BECAUSE NO TESTS RAN"
+  FAILED=1
 fi
 
 if  ! grep -q "All tests passed" "$TEMP_FILE"
 then
-  echo "One or more test have failed"
-  exit 1
+  echo "CI FAILED BECAUSE SOME TESTS FAILED"
+  echo ""
+  FAILED=1
 fi
 
 # Check for any error lines (case-insensitive)
 # Ignores null time for textures: https://github.com/godotengine/godot/issues/108994
-FILTERED_ERRORS=$(grep -i "ERROR" "$TEMP_FILE" | grep -v 'ERROR: Parameter "t" is null.') || true
+FILTERED_ERRORS=$(grep "ERROR" "$TEMP_FILE" | grep -v 'ERROR: Parameter "t" is null.') || true
 
 if [ -n "$FILTERED_ERRORS" ]; then
-  echo "Detected error output in test logs:"
+  echo "CI FAILED BECAUSE OF GODOT ERRORS"
   echo "$FILTERED_ERRORS"
-  exit 1
+  echo ""
+  FAILED=1
 fi
 
-echo "ALL GOOD :) :) :)"
+# Check for invalid UID warnings which will cause problems for other people
+FILTERED_WARNINGS=$(grep "WARNING" "$TEMP_FILE" | grep 'invalid UID:') || true
 
-exit 0
+if [ -n "$FILTERED_WARNINGS" ]; then
+  echo "CI FAILED BECAUSE OF GODOT UID WARNINGS"
+  echo "$FILTERED_WARNINGS"
+  echo ""
+  FAILED=1
+fi
+
+if [ "$FAILED" -eq 0 ]; then
+  echo "ALL GOOD :) :) :)"
+fi
+
+exit $FAILED
